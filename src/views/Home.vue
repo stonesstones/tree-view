9dbf2b718493db7fa3a89267de1e89e04d647aee
stonesstones <ku292stones@gmail.com>
2021-05-13 18:16:38 +0900

    input function have to be done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <leaf
+  @
+  />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import leaf from '../components/leaf'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+      leaf,
+    },
   }
-}
 </script>
